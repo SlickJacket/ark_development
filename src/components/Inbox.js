8970f368ebc089119d415a7d06fac2cc72f59e7c
SlickJacket <novenarii@gmail.com>
2020-04-01 20:14:39 -0400

    added compose button to inbox page

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -4,12 +4,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import ReactSearchBox from 'react-search-box';
+import AddIcon from "@material-ui/icons/Add";
+import ReactSearchBox from "react-search-box";
 import theme from "./ui/Theme";
 import InboxTreeView from "./ui/InboxTreeView";
-import MailBox from "./ui/MailBox"
+import MailBox from "./ui/MailBox";
 
-import './App.css'
+import "./App.css";
 
 const permDrawerWidth = 240;
 
@@ -31,6 +32,9 @@ const useStyles = makeStyles(theme => ({
   mailContainer: {
     backgroundColor: "blue",
     textAlign: "center"
+  },
+  composeButton: {
+    width: "100%"
   }
 }));
 
@@ -39,42 +43,45 @@ export default function Inbox() {
 
   const data = [
     {
-      key: 'john',
-      value: 'John Doe',
+      key: "john",
+      value: "John Doe"
     },
     {
-      key: 'jane',
-      value: 'Jane Doe',
+      key: "jane",
+      value: "Jane Doe"
     },
     {
-      key: 'mary',
-      value: 'Mary Phillips',
+      key: "mary",
+      value: "Mary Phillips"
     },
     {
-      key: 'robert',
-      value: 'Robert',
+      key: "robert",
+      value: "Robert"
     },
     {
-      key: 'karius',
-      value: 'Karius',
-    },
-  ]
+      key: "karius",
+      value: "Karius"
+    }
+  ];
 
   return (
     <Grid className={classes.inboxPageContainer} container direction="row">
       <Grid item lg={12} className={classes.searchBarContainer}>
-      <ReactSearchBox
-        placeholder="Search"
-        data={data}
-        callback={record => console.log(record)}
-        dropDownHoverColor={theme.palette.common.red}
-      />
+        <ReactSearchBox
+          placeholder="Search"
+          data={data}
+          callback={record => console.log(record)}
+          dropDownHoverColor={theme.palette.common.red}
+        />
       </Grid>
       <Grid item className={classes.sideBarContainer} lg={4}>
-        <InboxTreeView/>
+        <Button variant="contained" color="secondary" startIcon={<AddIcon />} className={classes.composeButton}>
+          <Typography>Compose</Typography>
+        </Button>
+        <InboxTreeView />
       </Grid>
       <Grid item className={classes.mailContainer} lg={8}>
-        <MailBox/>
+        <MailBox />
       </Grid>
     </Grid>
   );
