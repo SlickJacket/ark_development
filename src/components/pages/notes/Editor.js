b416c8dd25b6b5d1258bcdb91b00c07549cc587f
SlickJacket <novenarii@gmail.com>
2020-04-08 13:53:28 -0400

    restructured note.js

--- src/components/pages/notes/Editor.js
+++ src/components/pages/notes/Editor.js
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import ToolTip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import SaveIcon from "@material-ui/icons/Save";
 
 import "../../App.css";
 
+const useStyles = makeStyles(theme => ({
+  editorTitle: {
+    backgroundColor: theme.palette.secondary.main,
+    height: "3em"
+  },
+  titleInput: {
+    color: "white",
+    fontSize: "24px",
+
+    "&&&:before": {
+      borderBottom: "none"
+    },
+    "&&:after": {
+      borderBottom: "none"
+    }
+  },
+  saveIcon: {
+    color: "white",
+    "&:hover": {
+      color: theme.palette.primary.main
+    }
+  }
+}));
+
 export default function Editor() {
+  const classes = useStyles();
   const [value, setValue] = useState("");
 
   const modules = {
@@ -44,12 +77,40 @@ export default function Editor() {
   ];
 
   return (
-    <ReactQuill
-      theme="snow"
-      value={value}
-      onChange={setValue}
-      modules={modules}
-      formats={formats}
-    />
+    <Paper className="notePaper">
+      <Grid container>
+        <Grid item xs={12}>
+          <Paper className={classes.editorTitle}>
+            <Grid container justify="space-around" alignItems="center">
+              <Grid item xs={4}>
+                <TextField
+                  placeholder="Title"
+                  InputProps={{
+                    className: classes.titleInput
+                  }}
+                  inputProps={{ maxLength: 30 }}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <ToolTip title="Save">
+                  <IconButton>
+                    <SaveIcon className={classes.saveIcon} />
+                  </IconButton>
+                </ToolTip>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            modules={modules}
+            formats={formats}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
--- src/components/pages/notes/Notes.js
+++ src/components/pages/notes/Notes.js
@@ -5,9 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
 import Editor from "./Editor";
-import SaveIcon from "@material-ui/icons/Save";
 import FolderIcon from "@material-ui/icons/Folder";
 import NoteAddIcon from "@material-ui/icons/NoteAdd";
 import CreateNewFolderIcon from "@material-ui/icons/CreateNewFolder";
@@ -38,35 +36,14 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing.unit
     // width: "90%"
   },
-  titleInput: {
-    color: "white",
-    fontSize: "24px",
-
-    "&&&:before": {
-      borderBottom: "none"
-    },
-    "&&:after": {
-      borderBottom: "none"
-    }
-  },
   contentTextField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit
-  },
-  editorTitle: {
-    backgroundColor: theme.palette.secondary.main,
-    height: "3em"
   },
   notesAndFolders: {
     height: "40em"
   },
   folderMenuIcons: {
-    "&:hover": {
-      color: theme.palette.primary.main
-    }
-  },
-  saveIcon: {
-    color: "white",
     "&:hover": {
       color: theme.palette.primary.main
     }
@@ -128,70 +105,8 @@ export default function Notes() {
         </Paper>
       </Grid>
       <Grid item xs={9}>
-        <Paper className="notePaper">
-          <Grid container>
-            <Grid item xs={12}>
-              <Paper className={classes.editorTitle}>
-                <Grid container justify="space-around" alignItems="center">
-                  <Grid item xs={4}>
-                    <TextField
-                      placeholder="Title"
-                      InputProps={{
-                        className: classes.titleInput
-                      }}
-                      inputProps={{ maxLength: 20 }}
-                    />
-                  </Grid>
-                  <Grid item xs={2}>
-                    <ToolTip title="Save">
-                      <IconButton>
-                        <SaveIcon className={classes.saveIcon} />
-                      </IconButton>
-                    </ToolTip>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
-            <Grid item xs={12}>
-              <Editor />
-            </Grid>
-          </Grid>
-        </Paper>
+        <Editor />
       </Grid>
     </Grid>
   );
 }
-
-// <Grid container direction="row" justify="flex-start">
-//             <Grid item xs={5}>
-//               <TextField
-//                 // label="Add a Note"
-//                 placeholder="Title"
-//                 variant="standard"
-//                 // rowsMax="4"
-//                 // onChange={handleChange("multiline")}
-//                 className={classes.titleTextField}
-//                 InputProps={{
-//                   classes: {
-//                     input: classes.titleInput
-//                   }
-//                 }}
-//                 inputProps={{maxLength: 20}}
-//                 margin="normal"
-//               />
-//             </Grid>
-//             <Grid item xs={11}>
-//               <TextField
-//                 id="standard-multiline-flexible"
-//                 // label="Add a Note"
-//                 placeholder="Add a Note"
-//                 variant="standard"
-//                 multiline
-//                 // rowsMax="4"
-//                 fullWidth
-//                 // onChange={handleChange("multiline")}
-//                 className={classes.contentTextField}
-//                 margin="normal"
-//               />
-//             </Grid>
-//           </Grid>
